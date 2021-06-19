0002dc9c3e086aa5b63244f23ff90fc8e29c9a6d
Kyle Rockman <krockman@underarmour.com>
2021-06-19 16:42:56 -0500

    use a shell script to get variable interpolation

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 env
 
 opslevel create deploy --logLevel=DEBUG \
-  -i="${INPUT_INTEGRATIONID}" \
-  -s="${INPUT_SERVICE:-${GITHUB_REPOSITORY}}" \
-  -e="${INPUT_ENVIRONMENT}" \
-  -n="${INPUT_NUMBER:-${GITHUB_RUN_NUMBER}}" \
-  --url="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" \
-  --id="${GITHUB_RUN_ID}"
+  -i "${INPUT_INTEGRATIONID}" \
+  -s "${INPUT_SERVICE:-${GITHUB_REPOSITORY}}" \
+  -e "${INPUT_ENVIRONMENT}" \
+  -n "${INPUT_NUMBER:-${GITHUB_RUN_NUMBER}}" \
+  --url "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" \
+  --id "${GITHUB_RUN_ID}"
