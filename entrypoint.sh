4efe8f7fe824c426bb583f8478977b83d3fa7112
Kyle Rockman <krockman@underarmour.com>
2021-06-19 17:13:41 -0500

    feature: add ability to read ./opslevel.yml for the service name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -l
+OPSLEVEL_SERVICE=
+OPSLEVEL_FILE=./opslevel.yml
+if test -f "$OPSLEVEL_FILE"; then
+  OPSLEVEL_SERVICE=$(cat ./opslevel.yml | grep "name:" | awk '{gsub("name: ",""); print}') 
+fi
 
 cat << EOF | opslevel create deploy -i "${INPUT_INTEGRATIONID}"
-service: "${INPUT_SERVICE:-${GITHUB_REPOSITORY}}"
+service: "${INPUT_SERVICE:-${OPSLEVEL_SERVICE:-${GITHUB_REPOSITORY}}}"
 environment: "${INPUT_ENVIRONMENT}"
 deployNumber: "${INPUT_NUMBER:-${GITHUB_RUN_NUMBER}}"
 deployUrl: "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
