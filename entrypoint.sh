b84a193d168c7d6d1a860bef2e95850eb10687d9
Kyle Rockman <krockman@underarmour.com>
2021-06-19 16:56:58 -0500

    cleanup entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-env
-
-opslevel create deploy --logLevel=DEBUG \
-  -i "${INPUT_INTEGRATIONID}" \
-  -s "${INPUT_SERVICE:-${GITHUB_REPOSITORY}}" \
-  -e "${INPUT_ENVIRONMENT}" \
-  -n "${INPUT_NUMBER:-${GITHUB_RUN_NUMBER}}" \
-  --url "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" \
-  --id "${GITHUB_RUN_ID}"
+cat << EOF | opslevel create deploy -i "${INPUT_INTEGRATIONID}"
+service: "${INPUT_SERVICE:-${GITHUB_REPOSITORY}}"
+environment: "${INPUT_ENVIRONMENT}"
+deployNumber: "${INPUT_NUMBER:-${GITHUB_RUN_NUMBER}}"
+deployUrl: "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+dedupId: "${GITHUB_RUN_ID}"
+deployer:
+  name: "${GITHUB_ACTOR}"
+EOF
