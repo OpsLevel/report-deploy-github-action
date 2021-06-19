feae7a638870dae0c57e083c0e4e019c1acb97de
Kyle <rocktavious@icloud.com>
2021-06-19 16:38:55 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+env
+
+opslevel create deploy --logLevel=DEBUG \
+  -i="${INPUT_INTEGRATIONID}" \
+  -s="${INPUT_SERVICE:-${GITHUB_REPOSITORY}}" \
+  -e="${INPUT_ENVIRONMENT}" \
+  -n="${INPUT_NUMBER:-${GITHUB_RUN_NUMBER}}" \
+  --url="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" \
+  --id="${GITHUB_RUN_ID}"
