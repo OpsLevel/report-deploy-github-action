e0959ec4a11bbb0dc7ed8ce9577a91c016bb7682
David Bloss <david@opslevel.com>
2024-08-19 06:28:52 -0700

    convert from Docker to Composite Github Action (#56)
    * convert from Docker to Composite Github Action
    
    * send curl request via created json file
    
    * add entrypoint.sh back in

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM public.ecr.aws/opslevel/cli:v2024.1.16
-ENTRYPOINT ["/entrypoint.sh"]
-COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,17 @@ if test -f "$OPSLEVEL_FILE"; then
 fi
 
 cat <<EOF > data.yaml
-service: "${INPUT_SERVICE:-${OPSLEVEL_SERVICE:-${GITHUB_REPOSITORY}}}"
-description: "${INPUT_DESCRIPTION}"
-environment: "${INPUT_ENVIRONMENT:-production}"
-deploy-number: "${INPUT_NUMBER:-${GITHUB_RUN_NUMBER}}"
+service: "${SERVICE:-${OPSLEVEL_SERVICE}}"
+description: "${DESCRIPTION}"
+environment: "${ENVIRONMENT}"
+deploy-number: "${NUMBER}"
 deploy-url: "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
-dedup-id: "${INPUT_DEDUPLICATION_ID:-${GITHUB_RUN_ID}}"
+dedup-id: "${DEDUPLICATION_ID}"
 deployer:
-  name: "${INPUT_DEPLOYER_NAME:-${GITHUB_ACTOR}}"
-  email: "${INPUT_DEPLOYER_EMAIL}"
+  name: "${DEPLOYER_NAME}"
+  email: "${DEPLOYER_EMAIL}"
 EOF
 
-opslevel create deploy -i "${INPUT_INTEGRATION_URL}" -f .
+opslevel create deploy -i "$INTEGRATION_URL" -f .
 
 rm data.yaml
